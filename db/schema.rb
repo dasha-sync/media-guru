1a1d8b005828c2f3bb9c816721069e4a0a045df6
Darya Kapoor <kapoor.darya@gmail.com>
2024-01-20 15:03:43 +0300

    to sq

--- db/schema.rb
+++ db/schema.rb
@@ -10,9 +10,19 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2023_12_20_143322) do
+ActiveRecord::Schema[7.0].define(version: 2023_12_21_141704) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
+
+  create_table "films", force: :cascade do |t|
+    t.string "film_name", null: false
+    t.text "film_description"
+    t.date "release_date"
+    t.string "film_link"
+    t.string "image_link"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
 
   create_table "users", force: :cascade do |t|
     t.string "email", default: "", null: false
