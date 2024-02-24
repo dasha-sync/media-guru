93082822b809cab12b99024433ff73959b686c73
daria <kapoor.darya@gmai.com>
2024-02-24 11:19:09 +0300

    Adding favorites

--- app/controllers/favorites_controller.rb
+++ app/controllers/favorites_controller.rb
@@ -1,11 +1,10 @@
 # frozen_string_literal: true
 
 class FavoritesController < ApplicationController
-  before_action :set_video
   before_action :set_favorite, only: %i[edit show update destroy]
 
   def index
-    @favorites = @video.favorites
+    @favorites = current_user.favorites
     @favorite = Favorite.new
   end
 
@@ -13,28 +12,6 @@ class FavoritesController < ApplicationController
 
   def new
     @favorite = Favorite.new
-  end
-
-  def edit; end
-
-  def create
-    @favorite = @video.favorites.new(favorite_params)
-
-    @favorite.user = current_user if current_user
-
-    if @favorite.save
-      redirect_to video_path(@video), notice: 'Видео успешно добавлено в избранные успешно добавлена.'
-    else
-      render :new, status: :unprocessable_entity, error: 'Видео не может быть добавлено в ихбранные по какой-то причине.'
-    end
-  end
-
-  def update
-    if @favorite.update(favorite_params)
-      redirect_to video_path(@video), notice: 'Оценка успешно обновлен'
-    else
-      render :edit, status: :unprocessable_entity, error: 'Оценка не может быть обновлена по какой-то причине.'
-    end
   end
 
   def destroy
@@ -47,15 +24,7 @@ class FavoritesController < ApplicationController
 
   private
 
-  def set_video
-    @video = Video.find(params[:video_id])
-  end
-
   def set_favorite
     @favorite = current_user.favorites.find(params[:id])
-  end
-
-  def favorite_params
-    params.require(:favorite).permit()
   end
 end
--- app/controllers/videos_controller.rb
+++ app/controllers/videos_controller.rb
@@ -1,7 +1,7 @@
 # frozen_string_literal: true
 
 class VideosController < ApplicationController
-  before_action :set_video, only: %i[edit show update destroy]
+  before_action :set_video, only: %i[edit show update destroy add_to_favorites delete_from_favorites delete_from_videos_favorites]
 
   def index
     @videos = Video.all
@@ -22,25 +22,52 @@ class VideosController < ApplicationController
     @video = Video.new(video_params)
 
     if @video.save
-      redirect_to videos_path, notice: 'Фильм успешно добавлен.'
+      redirect_to videos_path, notice: 'Видео успешно добавлено.'
     else
-      render :new, status: :unprocessable_entity, error: 'Фильм не может быть добавлен по какой-то причине.'
+      render :new, status: :unprocessable_entity, error: 'Видео не может быть добавлено по какой-то причине.'
     end
   end
 
   def update
     if @video.update(video_params)
-      redirect_to videos_path(@video), notice: 'Фильм успешно обновлен.'
+      redirect_to videos_path(@video), notice: 'Видео успешно обновлен.'
     else
-      render :edit, status: :unprocessable_entity, error: 'Фильм не может быть обновлен по какой-то причине.'
+      render :edit, status: :unprocessable_entity, error: 'Видео не может быть обновлено по какой-то причине.'
     end
   end
 
   def destroy
     if @video.destroy
-      redirect_to videos_path, notice: 'Фильм успешно удален.'
+      redirect_to videos_path, notice: 'Видео успешно удалено.'
     else
-      redirect_to videos_path, error: 'Фильм не может быть удален по какой-то причине'
+      redirect_to videos_path, error: 'Видео не может быть удалено по какой-то причине'
+    end
+  end
+
+  def add_to_favorites
+    @favorite = current_user.favorites.new()
+    @favorite.video_id = @video.id
+
+    if @favorite.save
+      redirect_to videos_path, notice: 'Видео успешно добавлено в избранные.'
+    else
+      redirect_to videos_path, notice: 'Видео не может быть добавлено в избранные по какойто причине.'
+    end
+  end
+
+  def delete_from_videos_favorites
+    if deleting(@video)
+      redirect_to videos_path, notice: 'Видео успешно удалено из избранных.'
+    else
+      redirect_to videos_path, notice: 'Видео не может быть удалено из избранных по какойто причине.'
+    end
+  end
+
+  def delete_from_favorites
+    if deleting(@video)
+      redirect_to favorites_path, notice: 'Видео успешно удалено из избранных.'
+    else
+      redirect_to favorites_path, notice: 'Видео не может быть удалено из избранных по какойто причине.'
     end
   end
 
@@ -52,5 +79,11 @@ class VideosController < ApplicationController
 
   def video_params
     params.require(:video).permit(:video_name, :video_description, :release_date, :picture_url, :video_url)
+  end
+
+  def deleting(current_video)
+    Favorite.favorite_exist(current_video.id, current_user).each do |favorite|
+      return false unless favorite.delete
+    end
   end
 end
